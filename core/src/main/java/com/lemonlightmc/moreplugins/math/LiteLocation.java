0f996f385ce22aeec3206882d49391852a31362d
BruderJulian <julian@familie-zech.de>
2025-12-01 22:52:50 +0100

    added Locations Helper Classes

---
+++ core/src/main/java/com/lemonlightmc/moreplugins/math/LiteLocation.java
@@ -0,0 +1,169 @@
+package com.lemonlightmc.moreplugins.math;
+
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+
+import com.lemonlightmc.moreplugins.wrapper.Cloneable;
+
+public class LiteLocation implements Cloneable<LiteLocation> {
+  private Reference<World> world;
+  private double x;
+  private double y;
+  private double z;
+  private float pitch;
+  private float yaw;
+
+  public LiteLocation(final World world, final double x, final double y, final double z, final float yaw,
+      final float pitch) {
+    if (world != null) {
+      this.world = new WeakReference<>(world);
+    }
+
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.pitch = pitch;
+    this.yaw = yaw;
+  }
+
+  public LiteLocation(final World world, final double x, final double y, final double z) {
+    this(world, x, y, z, 0, 0);
+  }
+
+  public LiteLocation(final World world) {
+    this.world = (world == null) ? null : new WeakReference<>(world);
+  }
+
+  public World getWorld() {
+    if (this.world == null) {
+      return null;
+    }
+
+    final World world = this.world.get();
+    if (world == null) {
+      throw new IllegalArgumentException("World is unloaded");
+    }
+    return world;
+  }
+
+  public boolean isWorldLoaded() {
+    if (this.world == null) {
+      return false;
+    }
+
+    World world = this.world.get();
+    return world != null && world.equals(Bukkit.getWorld(world.getUID()));
+  }
+
+  public void setWorld(final Reference<World> world) {
+    this.world = world;
+  }
+
+  public double getX() {
+    return x;
+  }
+
+  public void setX(final double x) {
+    this.x = x;
+  }
+
+  public double getY() {
+    return z;
+  }
+
+  public void setY(final double y) {
+    this.y = y;
+  }
+
+  public double getZ() {
+    return z;
+  }
+
+  public void setZ(final double z) {
+    this.z = z;
+  }
+
+  public float getPitch() {
+    return pitch;
+  }
+
+  public void setPitch(final float pitch) {
+    this.pitch = pitch;
+  }
+
+  public float getYaw() {
+    return yaw;
+  }
+
+  public void setYaw(final float yaw) {
+    this.yaw = yaw;
+  }
+
+  public org.bukkit.Location toLocation() {
+    return new org.bukkit.Location(getWorld(), x, y, z, yaw, pitch);
+  }
+
+  public double distance(final LiteLocation o) {
+    return Math.sqrt(distanceSquared(o));
+  }
+
+  public double distanceSquared(final LiteLocation o) {
+    if (o == null) {
+      throw new IllegalArgumentException("Cannot measure distance to a null location");
+    } else if (o.getWorld() == null || getWorld() == null) {
+      throw new IllegalArgumentException("Cannot measure distance to a null world");
+    } else if (o.getWorld() != getWorld()) {
+      throw new IllegalArgumentException(
+          "Cannot measure distance between " + getWorld().getName() + " and " + o.getWorld().getName());
+    }
+
+    return (x - o.x) * (x - o.x) + (y - o.y) * (y - o.y) + (z - o.z) * (z - o.z);
+  }
+
+  public LiteLocation clone() {
+    try {
+      return (LiteLocation) super.clone();
+    } catch (final CloneNotSupportedException e) {
+      throw new Error(e);
+    }
+  }
+
+  @Override
+  public int hashCode() {
+    int result = 31 + ((world == null) ? 0 : world.hashCode());
+    result = 31 * result + Double.hashCode(x);
+    result = 31 * result + Double.hashCode(y);
+    result = 31 * result + Double.hashCode(z);
+    result = 31 * result + Float.floatToIntBits(pitch);
+    return 31 * result + Float.floatToIntBits(yaw);
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    final LiteLocation other = (LiteLocation) obj;
+    if (world == null) {
+      if (other.world != null)
+        return false;
+    }
+    return world.equals(other.world) && Double.doubleToLongBits(x) == Double.doubleToLongBits(other.x)
+        && Double.doubleToLongBits(y) == Double.doubleToLongBits(other.y)
+        && Double.doubleToLongBits(z) == Double.doubleToLongBits(other.z)
+        && Float.floatToIntBits(pitch) == Float.floatToIntBits(other.pitch)
+        && Float.floatToIntBits(yaw) == Float.floatToIntBits(other.yaw);
+  }
+
+  @Override
+  public String toString() {
+    return "LiteLocation [world=" + world + ", x=" + x + ", y=" + y + ", z=" + z + ", pitch=" + pitch + ", yaw=" + yaw
+        + "]";
+  }
+}
---
+++ core/src/main/java/com/lemonlightmc/moreplugins/math/Location2D.java
@@ -0,0 +1,226 @@
+package com.lemonlightmc.moreplugins.math;
+
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+import org.bukkit.util.NumberConversions;
+import com.lemonlightmc.moreplugins.wrapper.Cloneable;
+
+public class Location2D implements Cloneable<Location2D>, ConfigurationSerializable {
+  private Reference<World> world;
+  private double x;
+  private double z;
+  private float pitch;
+  private float yaw;
+
+  public Location2D(World world, double x, double z, float yaw, float pitch) {
+    if (world != null) {
+      this.world = new WeakReference<>(world);
+    }
+
+    this.x = x;
+    this.z = z;
+    this.pitch = pitch;
+    this.yaw = yaw;
+  }
+
+  public Location2D(World world, double x, double z) {
+    this(world, x, z, 0, 0);
+  }
+
+  public Location2D(World world) {
+    this.world = (world == null) ? null : new WeakReference<>(world);
+  }
+
+  protected static int locToBlock(double loc) {
+    return NumberConversions.floor(loc);
+  }
+
+  public World getWorld() {
+    if (this.world == null) {
+      return null;
+    }
+
+    World world = this.world.get();
+    if (world == null) {
+      throw new IllegalArgumentException("World is unloaded");
+    }
+    return world;
+  }
+
+  public void setWorld(Reference<World> world) {
+    this.world = world;
+  }
+
+  public boolean isWorldLoaded() {
+    if (this.world == null) {
+      return false;
+    }
+
+    World world = this.world.get();
+    return world != null && world.equals(Bukkit.getWorld(world.getUID()));
+  }
+
+  public org.bukkit.Chunk getChunk() {
+    return getWorld().getChunkAt((int) x, (int) z);
+  }
+
+  public org.bukkit.block.Block getBlock() {
+    return getWorld().getBlockAt((int) x, 0, (int) z);
+  }
+
+  public double getX() {
+    return x;
+  }
+
+  public void setX(double x) {
+    this.x = x;
+  }
+
+  public int getBlockX() {
+    return locToBlock(x);
+  }
+
+  public double getZ() {
+    return z;
+  }
+
+  public void setZ(double z) {
+    this.z = z;
+  }
+
+  public int getBlockZ() {
+    return locToBlock(z);
+  }
+
+  public float getPitch() {
+    return pitch;
+  }
+
+  public void setPitch(float pitch) {
+    this.pitch = pitch;
+  }
+
+  public float getYaw() {
+    return yaw;
+  }
+
+  public void setYaw(float yaw) {
+    this.yaw = yaw;
+  }
+
+  public org.bukkit.Location toLocation() {
+    return new org.bukkit.Location(getWorld(), x, 0, z, yaw, pitch);
+  }
+
+  public double length() {
+    return Math.sqrt(lengthSquared());
+  }
+
+  public double lengthSquared() {
+    return NumberConversions.square(x) + NumberConversions.square(z);
+  }
+
+  public double distance(Location2D o) {
+    return Math.sqrt(distanceSquared(o));
+  }
+
+  public double distanceSquared(Location2D o) {
+    if (o == null) {
+      throw new IllegalArgumentException("Cannot measure distance to a null location");
+    } else if (o.getWorld() == null || getWorld() == null) {
+      throw new IllegalArgumentException("Cannot measure distance to a null world");
+    } else if (o.getWorld() != getWorld()) {
+      throw new IllegalArgumentException(
+          "Cannot measure distance between " + getWorld().getName() + " and " + o.getWorld().getName());
+    }
+
+    return NumberConversions.square(x - o.x) + NumberConversions.square(z - o.z);
+  }
+
+  public Location2D zero() {
+    x = 0;
+    z = 0;
+    return this;
+  }
+
+  public Location2D add(Location2D vec) {
+    if (vec == null || vec.getWorld() != getWorld()) {
+      throw new IllegalArgumentException("Cannot add Locations of differing worlds");
+    }
+
+    x += vec.x;
+    z += vec.z;
+    return this;
+  }
+
+  public Location2D add(double x, double y, double z) {
+    this.x += x;
+    this.z += z;
+    return this;
+  }
+
+  public Location2D subtract(Location2D vec) {
+    if (vec == null || vec.getWorld() != getWorld()) {
+      throw new IllegalArgumentException("Cannot add Locations of differing worlds");
+    }
+
+    x -= vec.x;
+    z -= vec.z;
+    return this;
+  }
+
+  public Location2D subtract(double x, double y, double z) {
+    this.x -= x;
+    this.z -= z;
+    return this;
+  }
+
+  public Location2D multiply(double m) {
+    x *= m;
+    z *= m;
+    return this;
+  }
+
+  public Location2D clone() {
+    try {
+      return (Location2D) super.clone();
+    } catch (CloneNotSupportedException e) {
+      throw new Error(e);
+    }
+  }
+
+  public Map<String, Object> serialize() {
+    HashMap<String, Object> data = new HashMap<String, Object>();
+
+    if (this.world != null) {
+      data.put("world", getWorld().getName());
+    }
+
+    data.put("x", this.x);
+    data.put("z", this.z);
+    data.put("yaw", this.yaw);
+    data.put("pitch", this.pitch);
+
+    return data;
+  }
+
+  public static Location2D deserialize(Map<String, Object> args) {
+    World world = null;
+    if (args.containsKey("world")) {
+      world = Bukkit.getWorld((String) args.get("world"));
+      if (world == null) {
+        throw new IllegalArgumentException("unknown world");
+      }
+    }
+
+    return new Location2D(world, NumberConversions.toDouble(args.get("x")),
+        NumberConversions.toDouble(args.get("z")), NumberConversions.toFloat(args.get("yaw")),
+        NumberConversions.toFloat(args.get("pitch")));
+  }
+}
