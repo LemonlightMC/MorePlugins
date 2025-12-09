ae7745b66fdd0153e88d9d4eb7861b4b87b5d1e9
BruderJulian <julian@familie-zech.de>
2025-12-09 19:45:45 +0100

    added CountDown / ConfirmRequest Util

--- core/src/main/java/com/lemonlightmc/moreplugins/wrapper/Countdown.java
+++ core/src/main/java/com/lemonlightmc/moreplugins/wrapper/Countdown.java
@@ -1,6 +1,7 @@
 package com.lemonlightmc.moreplugins.wrapper;
 
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 import com.lemonlightmc.moreplugins.base.PluginBase;
 import com.lemonlightmc.moreplugins.interfaces.Builder;
@@ -18,49 +19,75 @@ public class Countdown {
   private final Consumer<CountdownInfo> count;
   private final Consumer<CountdownInfo> done;
   private final Consumer<CountdownInfo> start;
-  private final long duration;
+  private final Supplier<Long> duration;
 
   private long startTime = -1L;
   private ScheduledTask task;
+  private long durationTime;
 
   public Countdown(final Consumer<CountdownInfo> done) {
     this(done, (i) -> {
     }, (i) -> {
-    }, DEFAULT_TIME);
+    }, () -> DEFAULT_TIME);
   }
 
   public Countdown(final Consumer<CountdownInfo> done, final Consumer<CountdownInfo> count) {
     this(done, count, (i) -> {
-    }, DEFAULT_TIME);
+    }, () -> DEFAULT_TIME);
   }
 
   public Countdown(final Consumer<CountdownInfo> done, final Consumer<CountdownInfo> count,
       final Consumer<CountdownInfo> start) {
-    this(done, count, start, DEFAULT_TIME);
+    this(done, count, start, () -> DEFAULT_TIME);
   }
 
   public Countdown(final Consumer<CountdownInfo> done, final Consumer<CountdownInfo> count,
-      final Consumer<CountdownInfo> start, final long ticks) {
+      final Consumer<CountdownInfo> start, final Supplier<Long> duration) {
     this.count = count;
     this.done = done;
     this.start = start;
-    this.duration = ticks;
+    this.duration = duration;
   }
 
-  public long getTotalTime() {
+  public Countdown(final Consumer<CountdownInfo> done, final Consumer<CountdownInfo> count,
+      final Consumer<CountdownInfo> start, final long duration) {
+    this.count = count;
+    this.done = done;
+    this.start = start;
+    this.duration = () -> duration;
+    this.durationTime = duration;
+  }
+
+  public Consumer<CountdownInfo> getDoneAction() {
+    return done;
+  }
+
+  public Consumer<CountdownInfo> getCountAction() {
+    return count;
+  }
+
+  public Consumer<CountdownInfo> getStartAction() {
+    return start;
+  }
+
+  public Supplier<Long> getDurationSupplier() {
     return duration;
   }
 
-  public long getStartTime() {
-    return startTime;
+  public Long getTotalTime() {
+    return task == null ? null : durationTime;
   }
 
-  public long getEndTime() {
-    return startTime + duration;
+  public Long getStartTime() {
+    return task == null ? null : startTime;
   }
 
-  public long getTimeLeft() {
-    return startTime + duration - System.currentTimeMillis();
+  public Long getEndTime() {
+    return task == null ? null : startTime + durationTime;
+  }
+
+  public Long getTimeLeft() {
+    return task == null ? null : startTime + durationTime - System.currentTimeMillis();
   }
 
   public boolean hasStarted() {
@@ -72,6 +99,7 @@ public class Countdown {
   }
 
   public void start() {
+    this.durationTime = duration.get();
     this.startTime = System.currentTimeMillis();
     start.accept(CountdownInfo.from(this));
     task = PluginBase.getInstanceScheduler().runEveryAsync(() -> {
@@ -95,9 +123,10 @@ public class Countdown {
     int result = 31 + ((count == null) ? 0 : count.hashCode());
     result = 31 * result + ((done == null) ? 0 : done.hashCode());
     result = 31 * result + ((start == null) ? 0 : start.hashCode());
-    result = 31 * result + (int) (duration ^ (duration >>> 32));
+    result = 31 * result + ((duration == null) ? 0 : duration.hashCode());
     result = 31 * result + (int) (startTime ^ (startTime >>> 32));
     result = 31 * result + ((task == null) ? 0 : task.hashCode());
+    result = 31 * result + (int) (durationTime ^ (durationTime >>> 32));
     return result;
   }
 
@@ -111,23 +140,25 @@ public class Countdown {
     }
     final Countdown other = (Countdown) obj;
     if (done == null && other.done != null || count == null && other.count != null
-        || start == null && other.start != null || task == null && other.task != null) {
+        || start == null && other.start != null || task == null && other.task != null
+        || duration == null && other.duration != null) {
       return false;
     }
-    return duration == other.duration && startTime == other.startTime && done.equals(other.done)
-        && count.equals(other.count) && start.equals(other.start) && task.equals(other.task);
+    return durationTime == other.durationTime && startTime == other.startTime && done.equals(other.done)
+        && count.equals(other.count) && start.equals(other.start) && duration.equals(other.duration)
+        && task.equals(other.task);
   }
 
   @Override
   public String toString() {
-    return "Countdown [duration=" + duration + ", startTime=" + startTime + "]";
+    return "Countdown [startTime=" + startTime + ", durationTime=" + durationTime + "]";
   }
 
   public static class CountdownBuilder implements Builder<Countdown> {
     private Consumer<CountdownInfo> count;
     private Consumer<CountdownInfo> done;
     private Consumer<CountdownInfo> start;
-    private long time = DEFAULT_TIME;
+    private Supplier<Long> duration = () -> DEFAULT_TIME;
 
     public CountdownBuilder() {
     }
@@ -147,18 +178,59 @@ public class Countdown {
       return this;
     }
 
-    public CountdownBuilder time(final long ticks) {
-      time = ticks;
+    public CountdownBuilder duration(final long ticks) {
+      this.duration = () -> ticks;
       return this;
     }
 
-    public CountdownBuilder time(final long ticks, final IPolyTimeUnit unit) {
-      time = unit.toMillis(ticks);
+    public CountdownBuilder duration(final long ticks, final IPolyTimeUnit unit) {
+      this.duration = () -> unit.toMillis(ticks);
+      return this;
+    }
+
+    public CountdownBuilder duration(final Supplier<Long> duration) {
+      this.duration = duration;
       return this;
     }
 
     public Countdown build() {
-      return new Countdown(done, count, start, time);
+      if (done == null || count == null) {
+        throw new IllegalArgumentException("No Action for the Countdown has be given!");
+      }
+      return new Countdown(done, count, start, duration);
+    }
+
+    @Override
+    public int hashCode() {
+      int result = 1;
+      result = 31 * result + ((count == null) ? 0 : count.hashCode());
+      result = 31 * result + ((done == null) ? 0 : done.hashCode());
+      result = 31 * result + ((start == null) ? 0 : start.hashCode());
+      result = 31 * result + ((duration == null) ? 0 : duration.hashCode());
+      return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+      if (this == obj) {
+        return true;
+      }
+      if (obj == null || getClass() != obj.getClass()) {
+        return false;
+      }
+      final CountdownBuilder other = (CountdownBuilder) obj;
+      if (done == null && other.done != null || count == null && other.count != null
+          || start == null && other.start != null) {
+        return false;
+      }
+      return done.equals(other.done)
+          && count.equals(other.count) && start.equals(other.start) && duration.equals(other.duration);
+    }
+
+    @Override
+    public String toString() {
+      return "CountdownBuilder [count=" + count + ", done=" + done + ", start=" + start + ", duration=" + duration
+          + "]";
     }
   }
 
@@ -180,7 +252,7 @@ public class Countdown {
     }
 
     public static CountdownInfo from(final Countdown c) {
-      return new CountdownInfo(c.startTime, c.getEndTime(), c.duration);
+      return new CountdownInfo(c.startTime, c.getEndTime(), c.durationTime);
     }
   }
 }
