20efa9e63f8d70f27bc7ee602640a9a3fe11da09
BruderJulian <100963187+Bruderjulian@users.noreply.github.com>
2025-12-12 12:47:17 +0000

    refacted CommandSource

--- commands/src/main/java/com/lemonlightmc/moreplugins/commands/argumentsbase/CommandSource.java
+++ commands/src/main/java/com/lemonlightmc/moreplugins/commands/argumentsbase/CommandSource.java
@@ -3,22 +3,45 @@ package com.lemonlightmc.moreplugins.commands.argumentsbase;
 import org.bukkit.Location;
 import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Entity;;
+import org.bukkit.entity.Entity;
 
-public record CommandSource(CommandSender sender, Location location, Entity entity) {
-  public CommandSource(CommandSender sender) {
+public class CommandSource<S extends CommandSender> {
+  
+  private final T sender;
+  private final Location loc;
+  private final Entity entity;
+
+  public CommandSource(T sender, Location location, Entity entity) {
+    this.sender = sender;
+    this.loc = location;
+    this.entity = entity;
+  }
+
+  public CommandSource(T sender) {
     this(sender, getLocation(sender), getEntity(sender));
   }
 
-  private static Location getLocation(CommandSender sender) {
+  public T getSender() {
+    return sender;
+  }
+
+  public Location getLocation() {
+    return loc;
+  }
+  
+  public Entity getEntity() {
+    return entity;
+  }
+  
+  public static Location getLocation(CommandSender sender) {
     return switch (sender) {
       case Entity entity -> entity.getLocation();
       case BlockCommandSender block -> block.getBlock().getLocation();
-      default -> new Location(null, 0, 0, 0);
+      default -> null;
     };
   }
 
-  private static Entity getEntity(CommandSender sender) {
+  public static Entity getEntity(CommandSender sender) {
     return sender instanceof Entity entity ? entity : null;
   }
 }
