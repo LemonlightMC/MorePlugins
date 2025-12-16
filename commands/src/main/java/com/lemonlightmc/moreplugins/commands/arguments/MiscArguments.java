c8a4bc5b7102027620c72c82ef9fcb5f37780470
BruderJulian <julian@familie-zech.de>
2025-12-16 17:47:33 +0100

    removed FunctionArgument & added LootTableArgument

--- commands/src/main/java/com/lemonlightmc/moreplugins/commands/arguments/MiscArguments.java
+++ commands/src/main/java/com/lemonlightmc/moreplugins/commands/arguments/MiscArguments.java
@@ -17,6 +17,8 @@ import org.bukkit.block.data.BlockData;
 import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.loot.LootTable;
+import org.bukkit.loot.LootTables;
 import org.bukkit.potion.PotionEffectType;
 
 import com.lemonlightmc.moreplugins.commands.StringReader;
@@ -24,7 +26,6 @@ import com.lemonlightmc.moreplugins.commands.Utils;
 import com.lemonlightmc.moreplugins.commands.argumentsbase.Argument;
 import com.lemonlightmc.moreplugins.commands.argumentsbase.ArgumentType;
 import com.lemonlightmc.moreplugins.commands.argumentsbase.CommandArguments;
-import com.lemonlightmc.moreplugins.commands.argumentsbase.MCFunction;
 import com.lemonlightmc.moreplugins.commands.argumentsbase.ParticleData;
 import com.lemonlightmc.moreplugins.commands.exceptions.CommandExceptions.DynamicCommandException;
 import com.lemonlightmc.moreplugins.commands.exceptions.CommandSyntaxException;
@@ -441,9 +442,9 @@ public class MiscArguments {
         if (value.length == 0) {
           throw new Exception();
         }
-        Material mat = Objects.requireNonNull(Material.getMaterial(value[0]));
+        final Material mat = Objects.requireNonNull(Material.getMaterial(value[0]));
         if (value.length > 1) {
-          int amount = Integer.parseInt(value[1]);
+          final int amount = Integer.parseInt(value[1]);
           return new ItemStack(Material.getMaterial(value[0]), amount);
         }
         return new ItemStack(mat, 1);
@@ -660,21 +661,24 @@ public class MiscArguments {
     }
   }
 
-  public static class FunctionArgument extends Argument<MCFunction, FunctionArgument> {
+  public static class LootTableArgument extends Argument<LootTable, LootTableArgument> {
 
-    private static final DynamicCommandException<Dynamic1ExceptionFunktion> INVALID_FUNCTION = new DynamicCommandException<Dynamic1ExceptionFunktion>(
-        value -> "Invalid Function '" + value + "'");
+    public static final String[] NAMES = Utils.mapRegistry(LootTables.values());
 
-    public FunctionArgument(final String name) {
-      super(name, MCFunction.class, ArgumentType.FUNCTION);
+    private static final DynamicCommandException<Dynamic1ExceptionFunktion> INVALID_LOOTTABLE = new DynamicCommandException<Dynamic1ExceptionFunktion>(
+        value -> "Invalid Loot Table '" + value + "'");
+
+    public LootTableArgument(final String name) {
+      super(name, LootTable.class, ArgumentType.LOOT_TABLE);
+      withSuggestions(NAMES);
     }
 
-    public FunctionArgument getInstance() {
+    public LootTableArgument getInstance() {
       return this;
     }
 
     @Override
-    public MCFunction parseArgument(final CommandSource<CommandSender> source, final StringReader reader,
+    public LootTable parseArgument(final CommandSource<CommandSender> source, final StringReader reader,
         final String key,
         final CommandArguments previousArgs)
         throws CommandSyntaxException {
@@ -682,10 +686,11 @@ public class MiscArguments {
       String value = null;
       try {
         value = reader.readString();
-        return null;
+        return Objects.requireNonNull(Objects.requireNonNull(
+            LootTables.valueOf(Objects.requireNonNull(value))).getLootTable());
       } catch (final Exception e) {
         reader.setCursor(start);
-        throw INVALID_FUNCTION.createWithContext(reader, value);
+        throw INVALID_LOOTTABLE.createWithContext(reader, value);
       }
     }
 
@@ -702,7 +707,7 @@ public class MiscArguments {
 
     @Override
     public String toString() {
-      return "FunctionArgument []";
+      return "LootTableArgument []";
     }
   }
 }
