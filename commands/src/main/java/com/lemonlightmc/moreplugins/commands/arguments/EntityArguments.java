f3d0d45c7e598408cf7f1641b5aedccb54ef727d
BruderJulian <julian@familie-zech.de>
2026-01-22 22:14:52 +0100

    improved EntitySelectors

--- commands/src/main/java/com/lemonlightmc/moreplugins/commands/arguments/EntityArguments.java
+++ commands/src/main/java/com/lemonlightmc/moreplugins/commands/arguments/EntityArguments.java
@@ -1,7 +1,10 @@
 package com.lemonlightmc.moreplugins.commands.arguments;
 
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.profile.PlayerProfile;
 
@@ -51,4 +54,30 @@ public class EntityArguments {
     }
   }
 
+  @SuppressWarnings("rawtypes")
+  public static class EntitySelectorArgument extends Argument<List, EntitySelectorArgument> {
+
+    public EntitySelectorArgument(final String name) {
+      super(name, List.class, ArgumentType.ENTITY_SELECTOR);
+      withSuggestions(Suggestions.from((info) -> Bukkit.getOnlinePlayers().stream().map(p -> p.getName()).toList()));
+
+    }
+
+    @Override
+    public EntitySelectorArgument getInstance() {
+      return this;
+    }
+
+    @Override
+    public List<Entity> parseArgument(final CommandSource<CommandSender> source, final StringReader reader,
+        final String key)
+        throws CommandSyntaxException {
+      String str = reader.readString();
+      Player player = Bukkit.getPlayerExact(str);
+      if (player != null) {
+        return List.of(player);
+      }
+      return Bukkit.selectEntities(source.sender(), str);
+    }
+  }
 }
